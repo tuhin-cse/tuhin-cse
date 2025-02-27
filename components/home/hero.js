27afea2a42ff6b7f431065e4c3b7e5809bb11e2f
Md A Razzak <md.a.razzak.17@gmail.com>
2025-02-27 11:10:46 +0600

    Update Intro

--- components/home/hero.js
+++ components/home/hero.js
@@ -7,13 +7,14 @@ const Hero = () => {
     return (
         <div className="container py-12 relative">
             <div className="flex flex-col gap-6 relative text-center">
-                <h1 className="text-5xl sm:text-6xl font-bold">Hi, I am <span className="text-primary block pt-2.5 sm:inline">MD. A. Razzak</span></h1>
+                <h1 className="text-5xl sm:text-6xl font-bold">Hi, I am <span
+                    className="text-primary block pt-2.5 sm:inline">MD. A. Razzak</span></h1>
                 <h2 className="text-3xl sm:text-4xl font-bold">Full Stack Engineer</h2>
                 <p className="max-w-xl mx-auto text-sm sm:text-base">
                     A MERN Stack Developer with 4 years of experience,
                     I have specialized in creating efficient, scalable,
-                    and robust web applications using MongoDB, Express.js,
-                    React, and Node.js. My expertise in JavaScript, both on
+                    and robust web applications using Go, Gin, Node.js, Express.js, Nest.js,
+                    React, and Next.js. My expertise in JavaScript, both on
                     the front end and back end, has allowed me to streamline
                     the development process and reuse code effectively.</p>
                 <div className="flex justify-center items-center gap-6">
